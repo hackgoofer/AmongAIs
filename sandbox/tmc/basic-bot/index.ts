752f3914b2c6a9cacd503f6a014959ae7e325d83
Sasha <sashashengyt@gmail.com>
2024-03-03 01:19:15 -0800

    hack

--- sandbox/tmc/basic-bot/index.ts
+++ sandbox/tmc/basic-bot/index.ts
@@ -8,6 +8,7 @@ dotenv.config();
 const { GATHER_API_KEY, SPACE_ID } = process.env;
 const { OPENAI_API_KEY } = process.env;
 const { BOT_NAME } = process.env;
+const { GATHER_MAP_ID } = process.env;
 
 if (!GATHER_API_KEY) {
   throw new Error("Missing the GATHER_API_KEY in env");
@@ -21,14 +22,17 @@ if (!OPENAI_API_KEY) {
 if (!BOT_NAME) {
   throw new Error("Missing the BOT_NAME in env");
 }
+if (!GATHER_MAP_ID) {
+  throw new Error("Missing the MAP_ID in env");
+}
 
 // create openai client
 const openai = new OpenAI();
 
-async function chatCompletion(input: string) {
+async function chatCompletion(input: string, inventory_itmes: string[] = []) {
   const completion = await openai.chat.completions.create({
     messages: [
-      {"role": "system", "content": "You are a rad ai in a space station."},
+      {"role": "system", "content": `You are a rad ai in a space station. In your inventory, you have: ${inventory_itmes.join(', ')}.`},
       {"role": "user", "content": input},
     ],
     model: "gpt-4-turbo-preview",
@@ -57,7 +61,39 @@ interface ObjectHolder{
 }
 
 let objects:ObjectHolder = {};
+game.subscribeToEvent("playerInteractsWithObject", async (obj, context) => {
+  if(context.player!.itemString && objects[context.playerId!]){
+      let temp = objects[context.playerId!]
+      let newObj = {...temp.obj,
+          key: Object.keys(game.completeMaps[temp.mapId].objects!).length,
+          x: context.player!.x!,
+          y: context.player!.y!
+      }
+      game.setObject(temp.mapId, temp.obj!.id!, newObj)
+  }
 
+  console.log("Sasha")
+  const playerInteractsWithObject = obj.playerInteractsWithObject;
+  const objectKey = playerInteractsWithObject["key"]
+  console.log(context.player!.itemString)
+  console.log(objects[context.playerId!])
+  console.log("before inventory")
+  console.log(context.player!.inventory)
+  if (context.player!.inventory && context.map!.objects[objectKey]) {
+    context.player!.inventory.order[objectKey] = Object.keys(context.player!.inventory.items).length;
+    context.player!.inventory.items[objectKey] = context.map!.objects[objectKey];
+    console.log("added inventory: " + objectKey);
+  }
+  console.log("added inventory: " + objectKey)
+  console.log("end inventory")
+  console.log(context.player!.inventory)
+
+  // if(playerTriggersItem.closestObjectTemplate && playerTriggersItem.closestObjectTemplate === "Special Object"){
+  //     let {mapId, obj} = game.getObject(playerTriggersItem.closestObject!)!;
+  //     game.setItem(playerTriggersItem.closestObjectTemplate!, obj!.normal, context.playerId);
+  //     objects[context.playerId!] = {mapId, obj};
+  // }
+})
 
 const eventList = [
   "info", "warn", "error", "ready", "serverHeartbeat", "transactionStatus", "playerMoves", "playerSetsStatus", "playerSpotlights", "playerRings", "playerChats", "playerGhosts", 
@@ -89,7 +125,7 @@ const eventList = [
 
 eventList.forEach((event: any) => {
   game.subscribeToEvent(event as "info" | "warn" | "error" | "ready" | "serverHeartbeat" | "transactionStatus" | "playerMoves" | "playerSetsStatus" | "playerSpotlights" | "playerRings" | "playerChats" | "playerGhosts" | "playerEntersWhisper" | "playerLeavesWhisper" | "playerActivelySpeaks" | "playerSetsName" | "playerSetsTextStatus" | "playerSetsEmojiStatus" | "playerSetsAffiliation" | "playerExits" | "playerSetsIsSignedIn" | "spaceOverwrites" | "spaceIsClosed" | "playerEntersPortal" | "spaceSetsIdMapping" | "playerSetsLastActive" | "playerShootsConfetti" | "playerSetsEventStatus" | "playerSetsInConversation" | "playerSetsCurrentArea" | "playerSetsImagePointer" | "cookieFound" | "playerEntersWhisperV2" | "playerSetsGoKartId" | "mapSetDimensions" | "mapSetBackgroundImagePath" | "mapSetForegroundImagePath" | "mapSetSpawns" | "mapSetPortals" | "mapSetAnnouncer" | "mapSetAssets" | "mapSetName" | "mapSetMuteOnEntry" | "mapSetUseDrawnBG" | "mapSetWalls" | "mapSetFloors" | "mapSetAreas" | "mapSetSpawn" | "playerSetsIsAlone" | "playerJoins" | "mapSetEnabledChats" | "mapSetDescription" | "mapSetDecoration" | "mapSetTutorialTasks" | "mapSetMiniMapImagePath" | "spacePlaysSound" | "mapSetScript" | "playerSetsIsMobile" | "setScreenPointerServer" | "playerSetsEmoteV2" | "playerSetsFocusModeEndTime" | "spaceSetsSpaceMembers" | "spaceSetsSpaceUsers" | "customEvent" | "playerBlocks" | "playerUpdatesFocusModeStatus" | "playerNotifies" | "playerSetsItemString" | "playerSetsFollowTarget" | "playerRequestsToLead" | "playerSetsManualVideoSrc" | "playerSetsIsNpc" | "playerSetsSubtitle" | "mapCommitsChanges" | "mapMoveObject" | "playerEditsChatMessage" | "fxShakeObject" | "fxShakeCamera" | "playerSendsCommand" | "spaceRegistersCommand" | "speakerUpdatesSession" | "playerUpdatesInventory" | "spaceUpdatesItems" | "playerSetsVehicleId" | "playerSetsSpeedModifier" | "playerHighFives" | "spaceStopsSound" | "hipToBeSquare" | "playerCrafts" | "playerTriggersInventoryItem" | "playerSetsAllowScreenPointer" | "precomputedEnterLocation" | "gotRequestMute" | "playerSetsDeskInfo" | "mapSetNooks" | "dynamicGates" | "playerWaves" | "playerSetsPronouns" | "playerSetsTitle" | "playerSetsTimezone" | "playerSetsDescription" | "playerSetsPhone" | "playerSetsPersonalImageUrl" | "playerSetsProfileImageUrl" | "spaceSetsCapacity" | "spaceOverCapacityDeniesUser" | "playerSetsAway" | "mapSetCollisionsBits" | "playerSetsCity" | "playerSetsCountry" | "playerSetsStartDate" | "playerStartsRecording" | "accessRequestsUpdated" | "accessRequestRespondedTo" | "spaceSetsGuestPassStatuses" | "playerSetsAvailability" | "subscriptionsUpdated" | "spaceRolePermissionOverrideUpdated" | "playerSetsLastRaisedHand" | "playerSetsCurrentlyEquippedWearables" | "playerSetsDisplayEmail" | "mapDeleteObjectByKey" | "mapSetObjectsV2" | "playerInteractsWithObject" | "playerTriggersObject" | "chimeSetsUserInfo" | "playerChangesMaps", (data, _context) => {
-    console.log(`Sasha [Event] "${event}"`, data);
+    console.log(`SYSTEM [Event] "${event}"`, data);
   });
 });
 
@@ -110,17 +146,44 @@ game.subscribeToEvent("playerSetsEmoteV2", (data, _context) => {
   console.log('[Event] "playerSetsEmoteV2"', data);
 });
 
+// Function to get a player's inventory
+function getPlayerInventory(playerId: string) {
+  const players = game.getPlayersInMap(GATHER_MAP_ID)
+  const player = players[playerId]
+  if (player) {
+    return player.inventory;
+  } else {
+    console.log(`Player with id ${playerId} not found.`);
+    return null;
+  }
+}
+
+// Function to convert inventory items to their names
+function inventoryItemToNames(inventoryItems: any[]) {
+  return inventoryItems.map(item => item._name);
+}
+
+
+
 // listen for chats and move
 game.subscribeToEvent("playerChats", async (data, _context) => {
   console.log('[Event] "playerChats"', data);
   const message = data.playerChats;
+  const players = game.getPlayersInMap(GATHER_MAP_ID)
+  console.log("players", players);
+  const player = players.filter((player) => {
+    player.id === message.senderId;
+  })
+  console.log("player", player);
+  const inventory = player.inventory;
 
   if (message.senderId === CurrentPlayerId) {
     // don't respond to our own messages
     console.log("ignoring our own message");
     return;
   }
-
+  console.log("inventory", inventory);
+  
   console.log("messageType:", message.messageType);
   if (message.messageType === "DM") {
     // do something
@@ -143,13 +206,15 @@ game.subscribeToEvent("playerChats", async (data, _context) => {
         break;
       default: 
         game.setEmote("💭");
-        const completion = await chatCompletion(message.contents);
+        const senderId = message.senderId
+
+        const completion = await chatCompletion(message.contents, inventoryItemToNames(inventory.items));
         game.chat(message.senderId, [], "", { contents: `${completion.message.content}` });
         game.setEmote("");
     }
   } else if (message.recipient === "LOCAL_CHAT") {
     game.setEmote("💭");
-    const completion = await chatCompletion(message.contents);
+    const completion = await chatCompletion(message.contents, inventoryItemToNames(inventory.items))
     game.chat("LOCAL_CHAT", [], "", { contents: `${completion.message.content}`});
     game.setEmote("");
   }
